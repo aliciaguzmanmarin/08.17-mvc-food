6019f6a484c3ee09f4a7807b8d95aed3d595ab07
visual_community@outlook.com <visual_community@outlook.com>
2019-08-17 13:40:08 -0400

    solucion completa

---
+++ MVC-food/MVC-food/Models/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.Entity;
+
+namespace MVC_food.Models
+{
+    public class DataContext : DbContext
+    {
+        public DataContext() : base("DefaultConnection")
+        {
+                
+        }
+
+        public System.Data.Entity.DbSet<MVC_food.Models.Food> Foods { get; set; }
+    }
+}
